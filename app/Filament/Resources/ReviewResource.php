7abbbe3d7f7c5cdb978a8a24dc7bd1cca7b83d1a
Daalleee <dalemasan10@gmail.com>
2025-06-16 10:39:06 +0700

    fitur ulasan di buat komen atau tidak di pakai lagi

--- app/Filament/Resources/ReviewResource.php
+++ app/Filament/Resources/ReviewResource.php
@@ -17,61 +17,61 @@ use App\Filament\Resources\ReviewResource\Pages;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 use App\Filament\Resources\ReviewResource\RelationManagers;
 
-class ReviewResource extends Resource
-{
-    protected static ?string $model = Review::class;
-    protected static ?string $navigationLabel = 'Ulasan';
-    protected static ?string $navigationIcon = 'heroicon-o-star';
-    protected static ?int $navigationSort = 4;
+// class ReviewResource extends Resource
+// {
+//     protected static ?string $model = Review::class;
+//     protected static ?string $navigationLabel = 'Ulasan';
+//     protected static ?string $navigationIcon = 'heroicon-o-star';
+//     protected static ?int $navigationSort = 4;
 
-    public static function form(Form $form): Form
-    {
-        return $form
-            ->schema([
-                Select::make('rental_id')->relationship('rental', 'id')->required(),
-                Select::make('user_id')->relationship('user', 'name')->required(),
-                TextInput::make('rating')->numeric()->minValue(1)->maxValue(5)->required(),
-                Textarea::make('comment')
-            ]);
-    }
+//     public static function form(Form $form): Form
+//     {
+//         return $form
+//             ->schema([
+//                 Select::make('rental_id')->relationship('rental', 'id')->required(),
+//                 Select::make('user_id')->relationship('user', 'name')->required(),
+//                 TextInput::make('rating')->numeric()->minValue(1)->maxValue(5)->required(),
+//                 Textarea::make('comment')
+//             ]);
+//     }
 
-    public static function table(Table $table): Table
-    {
-        return $table
-            ->columns([
-                TextColumn::make('user.name'),
-                TextColumn::make('rating'),
-                TextColumn::make('comment')->limit(30),
-                TextColumn::make('created_at')->dateTime(),
-            ])
-            ->filters([
-                //
-            ])
-            ->actions([
-                Tables\Actions\EditAction::make(),
-            ])
-            ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
-            ]);
-    }
+//     public static function table(Table $table): Table
+//     {
+//         return $table
+//             ->columns([
+//                 TextColumn::make('user.name'),
+//                 TextColumn::make('rating'),
+//                 TextColumn::make('comment')->limit(30),
+//                 TextColumn::make('created_at')->dateTime(),
+//             ])
+//             ->filters([
+//                 //
+//             ])
+//             ->actions([
+//                 Tables\Actions\EditAction::make(),
+//             ])
+//             ->bulkActions([
+//                 Tables\Actions\BulkActionGroup::make([
+//                     Tables\Actions\DeleteBulkAction::make(),
+//                 ]),
+//             ]);
+//     }
 
-    public static function getRelations(): array
-    {
-        return [];
-    }
+//     public static function getRelations(): array
+//     {
+//         return [];
+//     }
 
-    public static function getPages(): array
-    {
-        return [
-            'index' => Pages\ListReviews::route('/'),
-            // 'create' => Pages\CreateReview::route('/create'),
-            // 'edit' => Pages\EditReview::route('/{record}/edit'),
-        ];
-    }
-    // public static function canCreate(): bool
-    // {
-    //     return false;
-    // }
-}
+//     public static function getPages(): array
+//     {
+//         return [
+//             'index' => Pages\ListReviews::route('/'),
+//             // 'create' => Pages\CreateReview::route('/create'),
+//             // 'edit' => Pages\EditReview::route('/{record}/edit'),
+//         ];
+//     }
+//     // public static function canCreate(): bool
+//     // {
+//     //     return false;
+//     // }
+// }
